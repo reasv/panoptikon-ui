faddcf5da3f66ec87b1fc8895a3ae34da8573b78
reasv <7143787+reasv@users.noreply.github.com>
2024-08-28 03:43:49 +0200

    Scroll fix

--- app/search/SearchPageContent.tsx
+++ app/search/SearchPageContent.tsx
@@ -20,6 +20,7 @@ import { SearchQueryArgs } from "./page";
 import { SearchErrorToast } from "@/components/searchErrorToaster";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { cn } from "@/lib/utils";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function SearchPageContent({ initialQuery }:
     { initialQuery: SearchQueryArgs }
@@ -75,11 +76,14 @@ export function SearchPageContent({ initialQuery }:
     const isMobile = useMediaQuery("(max-width: 768px)")
     const isTablet = useMediaQuery("(max-width: 1024px)")
     const isSmallDesktop = useMediaQuery("(max-width: 1600px)")
-    const maxPagesButtons = isMobile ? 5 : isTablet ? 10 : isSmallDesktop ? 15 : 25
+    let maxPagesButtons = isMobile ? 5 : isTablet ? 10 : isSmallDesktop ? 15 : 25
+    if (advancedIsOpen) {
+        maxPagesButtons = isMobile ? 5 : isTablet ? 5 : isSmallDesktop ? 7 : 20
+    }
     return (
-        <div className="flex min-h-screen w-full">
+        <div className="flex w-full h-screen">
             <AdvancedSearchOptions />
-            <div className={cn('flex-grow p-4 transition-all duration-300 mx-auto',
+            <div className={cn('p-4 transition-all duration-300 mx-auto',
                 advancedIsOpen ? 'md:w-1/2 lg:w-1/2 xl:w-2/3 2xl:w-3/4' : 'md:w-full lg:w-full xl:w-full 2xl:w-full'
             )}>
                 <SearchErrorToast isError={isError} error={error} />
@@ -103,46 +107,49 @@ export function SearchPageContent({ initialQuery }:
                     </div>
                 </div>
 
-                <Card>
+                <Card className="flex-grow flex flex-col">
                     <CardHeader>
                         <CardTitle>
                             <AnimatedNumber value={nResults} /> {nResults === 1 ? "Result" : "Results"}
                         </CardTitle>
                     </CardHeader>
-                    <CardContent>
+                    <CardContent className="flex-grow overflow-hidden">
+                        <ScrollArea className="h-full max-h-[calc(100vh-250px)] overflow-y-auto" >
+                            <div className={cn('grid gap-4',
+                                advancedIsOpen ? 'sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 2xl:grid-cols-4' :
+                                    'sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5')}>
 
-                        <div className={cn('grid gap-4',
-                            advancedIsOpen ? 'md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 2xl:grid-cols-4' :
-                                'md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5')}>
-
-                            {data && data.results.map((result) => (
-                                <div key={result.path} className="border rounded p-2">
-                                    <div className="relative w-full pb-full mb-2 overflow-hidden group">
-                                        <a
-                                            href={`/api/items/file/${getFileURL(result.sha256)}`}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="block relative h-80 mb-2 overflow-hidden"
-                                        >
-                                            <Image
-                                                src={`/api/items/thumbnail/${getFileURL(result.sha256)}`}
-                                                alt={`Result ${result.path}`}
-                                                fill
-                                                className="object-cover transition-transform duration-300 hover:scale-105"
-                                                unoptimized={true}
-                                            />
-                                        </a>
-                                        <BookmarkBtn sha256={result.sha256} />
-                                        <OpenFile sha256={result.sha256} path={result.path} />
-                                        <OpenFolder sha256={result.sha256} path={result.path} />
+                                {data && data.results.map((result) => (
+                                    <div key={result.path} className="border rounded p-2">
+                                        <div className="overflow-hidden relative w-full pb-full mb-2 group">
+                                            <a
+                                                href={`/api/items/file/${getFileURL(result.sha256)}`}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={cn("block relative mb-2 h-80",
+                                                    advancedIsOpen ? 'sm:h-96 md:h-80 lg:h-96 xl:h-80 2xl:h-80' : 'sm:h-96 md:h-80 lg:h-96 xl:h-96 2xl:h-96'
+                                                )}
+                                            >
+                                                <Image
+                                                    src={`/api/items/thumbnail/${getFileURL(result.sha256)}`}
+                                                    alt={`Result ${result.path}`}
+                                                    fill
+                                                    className="object-cover transition-transform duration-300 hover:scale-105"
+                                                    unoptimized={true}
+                                                />
+                                            </a>
+                                            <BookmarkBtn sha256={result.sha256} />
+                                            <OpenFile sha256={result.sha256} path={result.path} />
+                                            <OpenFolder sha256={result.sha256} path={result.path} />
+                                        </div>
+                                        <FilePathComponent path={result.path} />
+                                        <p className="text-xs text-gray-500">
+                                            {new Date(result.last_modified).toLocaleString('en-US')}
+                                        </p>
                                     </div>
-                                    <FilePathComponent path={result.path} />
-                                    <p className="text-xs text-gray-500">
-                                        {new Date(result.last_modified).toLocaleString('en-US')}
-                                    </p>
-                                </div>
-                            ))}
-                        </div>
+                                ))}
+                            </div>
+                        </ScrollArea>
                     </CardContent>
                 </Card>
                 {data && data.count > page_size && (
