e15a27cfbacf8a635c3abdda220a9df7c649cdf6
reasv <7143787+reasv@users.noreply.github.com>
2024-08-31 00:34:05 +0200

    Fix click bug

--- components/sidebar/similarity/SimilarItemsSideBar.tsx
+++ components/sidebar/similarity/SimilarItemsSideBar.tsx
@@ -117,12 +117,16 @@ export function SimilarItemsView({
     }, {
         placeholderData: keepPreviousData
     })
+    const onImageClick = (index: number) => {
+        if (!data) return
+        const item = data.results[index]
+    }
     return (
         <div className="mt-4">
             {data && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 4xl:grid-cols-2 5xl:grid-cols-2 gap-4">
                     {data.results.map((result, index) => (
-                        <SearchResultImage key={index} result={result} index={index} dbs={dbs} imageContainerClassName="h-96 xl:h-80 4xl:h-80 5xl:h-80" />
+                        <SearchResultImage key={index} result={result} index={index} dbs={dbs} imageContainerClassName="h-96 xl:h-80 4xl:h-80 5xl:h-80" onImageClick={() => onImageClick(index)} />
                     ))}
                 </div>
             )}
