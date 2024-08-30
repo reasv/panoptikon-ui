f34f7d373f6c76facf9f7f460a1592e7998e0e15
reasv <7143787+reasv@users.noreply.github.com>
2024-08-31 01:16:50 +0200

    Remove unused imports

--- components/sidebar/similarity/SimilarItemsSideBar.tsx
+++ components/sidebar/similarity/SimilarItemsSideBar.tsx
@@ -1,19 +1,13 @@
 "use client"
 
-import { useDatabase, useDetailsPane, useImageSimilarity, useItemSelection } from "@/lib/zust"
+import { useDatabase, useImageSimilarity, useItemSelection } from "@/lib/zust"
 import { FilterContainer } from "../options/FilterContainer"
 import { components } from "@/lib/panoptikon"
-import { MultiBoxResponsive } from "@/components/multiCombobox"
 import { $api } from "@/lib/api"
 import { ConfidenceFilter } from "../options/confidenceFilter"
-import { Button } from "@/components/ui/button"
-import { Delete } from "lucide-react"
 import { keepPreviousData } from "@tanstack/react-query"
-import { Progress } from "@/components/ui/progress"
-import { useToast } from "@/components/ui/use-toast"
 import { SearchResultImage } from "@/components/SearchResultImage"
 import { ComboBoxResponsive } from "@/components/combobox"
-import { useState } from "react"
 
 export function SimilarItemsSideBar() {
     const selected = useItemSelection((state) => state.getSelected())
