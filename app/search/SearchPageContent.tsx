bae5595fa5b5d1338e42632460909a40ded944be
reasv <7143787+reasv@users.noreply.github.com>
2024-09-01 23:30:19 +0200

    move search hooks into file

--- app/search/SearchPageContent.tsx
+++ app/search/SearchPageContent.tsx
@@ -1,13 +1,11 @@
 "use client"
-import { $api } from "@/lib/api"
 import Image from 'next/image'
 import { PageSelect } from "@/components/pageselect";
 import { BookmarkBtn, FilePathComponent, OpenFile, OpenFolder } from "@/components/imageButtons"
-import { useInstantSearch, useSearchQuery } from "@/lib/state/zust"
+import { useInstantSearch } from "@/lib/state/zust"
 import { Toggle } from "@/components/ui/toggle"
 import { Settings, RefreshCw, X, ArrowBigLeft, ArrowBigRight, GalleryHorizontal } from "lucide-react"
 import { AnimatedNumber } from "@/components/ui/animatedNumber"
-import { keepPreviousData } from "@tanstack/react-query"
 import { InstantSearchLock } from "@/components/InstantSearchLock"
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
@@ -23,11 +21,11 @@ import { SideBar } from "@/components/sidebar/SideBar";
 import { OpenDetailsButton } from "@/components/OpenFileDetails";
 import { SearchResultImage } from "@/components/SearchResultImage";
 import { useItemSelection } from "@/lib/state/itemSelection";
-import { Mode, useSearchMode, useSimilarityQuery } from "@/lib/state/similarityQuery";
-import { useImageSimilarity } from "@/lib/state/similarityStore";
+import { Mode, useSearchMode } from "@/lib/state/similarityQuery";
 import { useGalleryIndex, useGalleryName, useGalleryThumbnail } from "@/lib/state/gallery";
 import { useSideBarOpen } from "@/lib/state/sideBar";
 import { useSelectedDBs } from "@/lib/state/database";
+import { useItemSimilaritySearch, useSearch } from "@/lib/searchHooks";
 
 export function SearchPageContent({ initialQuery }:
     { initialQuery: SearchQueryArgs }) {
@@ -93,103 +91,6 @@ export function MultiSearchView({ initialQuery }:
             }
         </>
     )
-}
-
-export function useItemSimilaritySearch() {
-    const [query, setQuery] = useSimilarityQuery()
-    const [dbs, ___] = useSelectedDBs()
-    const similarityQuery = useImageSimilarity(
-        (state) =>
-            query.is_type == "clip" ?
-                state.getClipQuery(query.is_model!)
-                :
-                state.getTextEmbedQuery(query.is_model!)
-    )
-    const instantSearch = useInstantSearch((state) => state.enabled)
-    const validQuery = !!(query.is_item && query.is_item.length > 0 && query.is_model && query.is_model.length > 0)
-    const [mode, _] = useSearchMode()
-    const { data, refetch, isFetching, isError, error } = $api.useQuery("post", "/api/search/similar/{sha256}", {
-        params: {
-            query: {
-                ...dbs
-            },
-            path: {
-                sha256: query.is_item || "",
-            }
-        },
-        body: {
-            ...similarityQuery,
-            setter_name: query.is_model!,
-            page: query.is_page,
-            page_size: similarityQuery.page_size,
-            full_count: true
-        }
-    },
-        {
-            enabled: instantSearch && validQuery && mode === Mode.ItemSimilarity,
-            placeholderData: keepPreviousData
-        }
-    )
-
-
-    const nResults = data?.count || 0
-    const page = query.is_page
-    const pageSize = similarityQuery.page_size
-    const setPage = (page: number) => setQuery({ is_page: page })
-    return {
-        data,
-        error,
-        isError,
-        refetch,
-        isFetching,
-        nResults,
-        page,
-        pageSize,
-        setPage,
-        searchEnabled: validQuery
-    }
-}
-
-export function useSearch({ initialQuery }:
-    { initialQuery: SearchQueryArgs }) {
-    const isClient = typeof window !== "undefined"
-    const searchQuery = isClient ? useSearchQuery((state) => state.getSearchQuery()) : initialQuery.body
-    const dbs = isClient ? useSelectedDBs()[0] : initialQuery.params.query
-    const setPage = useSearchQuery((state) => state.setPage)
-    const page = useSearchQuery((state) => state.order_args.page)
-    const pageSize = useSearchQuery((state) => state.order_args.page_size)
-    const searchEnabled = useSearchQuery((state) => state.enable_search)
-    const instantSearch = useInstantSearch((state) => state.enabled)
-    const [mode, _] = useSearchMode()
-    const { data, error, isError, refetch, isFetching } = $api.useQuery(
-        "post",
-        "/api/search",
-        {
-            params: {
-                query: dbs,
-            },
-            body: {
-                ...searchQuery
-            }
-        },
-        {
-            enabled: searchEnabled && instantSearch && mode === Mode.Search,
-            placeholderData: keepPreviousData
-        }
-    );
-    const nResults = data?.count || 0
-    return {
-        data,
-        error,
-        isError,
-        refetch,
-        isFetching,
-        nResults,
-        page,
-        pageSize,
-        setPage,
-        searchEnabled
-    }
 }
 
 export function SearchViewBar({
