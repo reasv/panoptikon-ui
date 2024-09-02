686f8bc92f4a628cee0ab79ceaff1d5ec775c5b2
reasv <7143787+reasv@users.noreply.github.com>
2024-09-02 03:16:26 +0200

    Prevent image overflow

--- components/sidebar/options/similarityTarget.tsx
+++ components/sidebar/options/similarityTarget.tsx
@@ -10,10 +10,13 @@ import { getFullFileURL, getLocale, prettyPrintBytes, prettyPrintVideoDuration }
 import { useSelectedDBs } from "@/lib/state/database";
 import { useSimilarityQuery } from "@/lib/state/similarityQuery";
 import { SearchResultImage } from "@/components/SearchResultImage";
+import { Button } from "@/components/ui/button";
+import { useItemSelection } from "@/lib/state/itemSelection";
 
 export function SimilarityTargetItem({
 }: {
     }) {
+    const selected = useItemSelection((state) => state.getSelected())
     const [dbs, ___] = useSelectedDBs()
     const [query, setQuery] = useSimilarityQuery()
     const { data } = $api.useQuery("get", "/api/items/item/{sha256}", {
@@ -39,13 +42,19 @@ export function SimilarityTargetItem({
         path: file?.path || "",
         type: item?.type || "unknown",
     }
+    function switchTarget() {
+        if (!selected) return
+        setQuery({
+            is_item: selected?.sha256,
+        })
+    }
     return (
         <FilterContainer
             label={<span>Similarity Search Target</span>}
             description={<span>The file you're comparing against</span>}
             storageKey="similarity-item-details-open"
         >
-            {resultItem && <SearchResultImage className="mt-4" result={resultItem} index={0} dbs={dbs} />}
+            {resultItem && <SearchResultImage className="mt-4 grid grid-cols-1" result={resultItem} index={0} dbs={dbs} />}
             <div className="space-x-2 mt-4">
                 <p className="text-xs text-gray-500 mt-2">
                     <a href={getFullFileURL(query.is_item!, dbs)} target="_blank">Download Original File ({sizeString})</a>
@@ -56,6 +65,11 @@ export function SimilarityTargetItem({
                 {timeAddedString && <p className="text-xs text-gray-500 mt-2">
                     Added: {timeAddedString}
                 </p>}
+            </div>
+            <div className="space-x-2 mt-4">
+                <Button variant="outline" disabled={!selected} onClick={() => switchTarget()}>
+                    Switch Target
+                </Button>
             </div>
         </FilterContainer>
     )
