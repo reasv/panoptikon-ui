3e6a59d6a5e4fe6c9b2f07c03e08bf3d81981c30
reasv <7143787+reasv@users.noreply.github.com>
2024-09-03 14:13:00 +0200

    Make sidebar SSR compatible

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -52,12 +52,12 @@ function SideBarContent() {
 }
 
 export function SideBar() {
-    const isDesktop = useMediaQuery("(min-width: 1024px)")
+    const isMobile = useMediaQuery("(max-width: 1024px)")
     const [sidebarOpen, setSideBarOpen] = useSideBarOpen()
     if (!sidebarOpen) {
         return null
     }
-    if (isDesktop) {
+    if (!isMobile) {
         return (
             <div className="h-full lg:w-1/2 xl:w-1/3 2xl:w-1/4 4xl:w-[20%] 5xl:w-[18%] p-4 shadow-lg z-50 hidden lg:block">
                 <ScrollArea className="h-full">
