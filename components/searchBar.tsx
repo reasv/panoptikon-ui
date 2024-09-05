15fdecb9ebd62293141a2013d015963733a1d0f4
reasv <7143787+reasv@users.noreply.github.com>
2024-09-05 02:19:12 +0200

    Hook up search bar

--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,29 +1,27 @@
 "use client"
 import { Input } from "@/components/ui/input"
-import { useSearchQuery } from "@/lib/state/zust"
 import { useSQLite } from "@/lib/sqliteChecker"
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useMemo } from "react"
 import { Fts5ToggleButton } from "./FTS5Toggle"
 import { PLACEHOLDERS } from "@/lib/placeholders"
 import { ClearSearch } from "./ClearSearch"
 import { useTagCompletionEnabled } from "@/lib/enableTagsHook"
-import { TagAutoComplete, TagCompletionInput } from "./tagInput"
+import { TagAutoComplete } from "./tagInput"
 import { useToast } from "./ui/use-toast"
 import { Toggle } from "./ui/toggle"
 import { Tag } from "lucide-react"
-import { useTagCompletionSettings } from "@/lib/state/tagCompletion"
 import { cn } from "@/lib/utils"
+import { useAnyTextFilterOptions } from "@/lib/state/searchQuery/clientHooks"
+import { useSearchQuery } from "@/lib/state/zust"
 
 export function SearchBar({
     onSubmit,
 }: {
     onSubmit?: () => void
 }) {
-    const setAnyTextQuery = useSearchQuery((state) => state.setAnyTextQuery)
-    const anyTextQuery = useSearchQuery((state) => state.any_text.query)
-    const setEnabled = useSearchQuery((state) => state.setEnableSearch)
-    const rawFts5Match = useSearchQuery((state) => state.any_text.raw_fts5_match)
-    const syntaxChecker = useSQLite(rawFts5Match)
+    const [anyTextQuery, setAnyTextQuery] = useAnyTextFilterOptions()
+    const syntaxChecker = useSQLite(anyTextQuery.raw_fts5_match)
+    const [enabled, setEnabled] = useSearchQuery((state) => [state.enable_search, state.setEnableSearch])
     const checkInput = (query: string, fts5Enabled: boolean) => {
         if (query.length === 0) {
             setEnabled(true)
@@ -50,20 +48,20 @@ export function SearchBar({
     }, [syntaxChecker.error])
 
     useEffect(() => {
-        checkInput(anyTextQuery, rawFts5Match)
-    }, [rawFts5Match])
+        checkInput(anyTextQuery.query, anyTextQuery.raw_fts5_match)
+    }, [anyTextQuery.raw_fts5_match])
 
     const onFTS5Enable = (enabled: boolean) => {
-        checkInput(anyTextQuery, enabled)
+        checkInput(anyTextQuery.query, enabled)
     }
 
     const onTextInputChange = (match_string: string) => {
-        checkInput(match_string, rawFts5Match)
+        checkInput(match_string, anyTextQuery.raw_fts5_match)
         // Trigram search requires at least 3 characters
         if (match_string.length > 0 && match_string.length < 3) {
             setEnabled(false)
         }
-        setAnyTextQuery(match_string)
+        setAnyTextQuery({ query: match_string })
     }
 
     const placeholder = useMemo(() => {
@@ -83,7 +81,7 @@ export function SearchBar({
             }
         }
     }
-    const showError = syntaxChecker.error && anyTextQuery && rawFts5Match
+    const showError = syntaxChecker.error && anyTextQuery.query && anyTextQuery.raw_fts5_match
     return (
         <>
             <TagCompletionSwitch />
@@ -91,7 +89,7 @@ export function SearchBar({
                 {completionEnabled ?
                     <TagAutoComplete
                         placeholder={placeholder}
-                        value={anyTextQuery}
+                        value={anyTextQuery.query}
                         onChange={onTextInputChange}
                         onSubmit={onSubmit}
                         inputClassName="flex-grow"
@@ -101,7 +99,7 @@ export function SearchBar({
                     <Input
                         type="text"
                         placeholder={placeholder}
-                        value={anyTextQuery}
+                        value={anyTextQuery.query}
                         onChange={(e) => onTextInputChange(e.target.value)}
                         onKeyDown={handleKeyDown}
                         className="flex-grow"
