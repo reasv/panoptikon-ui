26166a5d7ddb94b01caa224b4d463c903a6e687c
reasv <7143787+reasv@users.noreply.github.com>
2024-09-27 22:35:12 +0200

    Handle order correctly in UI

--- components/sidebar/options/orderBy.tsx
+++ components/sidebar/options/orderBy.tsx
@@ -1,29 +1,41 @@
 import { Label } from "../../ui/label"
 import { ComboBoxResponsive } from "../../combobox";
-import { components } from "@/lib/panoptikon";
-import { useOrderArgs, useSearchQuery } from "@/lib/state/searchQuery/clientHooks";
+import { useOrderArgs, useOrderBy, useSearchQuery } from "@/lib/state/searchQuery/clientHooks";
 import { orderByType } from "@/lib/state/searchQuery/searchQueryKeyMaps";
 
 export function OrderBy() {
+    const orderByData = useOrderBy()
     const query = useSearchQuery()
     const [_, setOrderArgs] = useOrderArgs()
 
+    const filterOrderOptions = [
+        { value: "bookmark_time", label: "Bookmarking Time" },
+        { value: "match_at", label: "Flexible Search Match" },
+        { value: "match_text", label: "Text Match" },
+        { value: "match_path", label: "Path Match" },
+        { value: "match_tags_confidence", label: "Tag Match" },
+        { value: "search_semantic_text", label: "Semantic Text Match" },
+        { value: "search_semantic_image", label: "Semantic Image Match" },
+        { value: "search_item_similarity", label: "Item Similarity Match" },
+    ]
+
+    const availableFilterOrderOptions = filterOrderOptions.filter(
+        (opt) => orderByData.meta.available_filter_orders.includes(opt.value)
+    )
     let orderOptions: {
         value: NonNullable<orderByType>
         label: string
     }[] = [
             { value: "path", label: "Filename" },
             { value: "last_modified", label: "Last Modified" },
-            { value: "time_added", label: "Bookmarking Time" },
-            // { value: "rank_any_text", label: "Flexible Search Match" },
+            { value: "size", label: "Size" },
+            { value: "type", label: "Type" },
+            { value: "duration", label: "Duration" },
         ]
-
-    // if (!query.query.filters!.any_text?.path || !query.query.filters!.any_text?.extracted_text) {
-    //     orderOptions = orderOptions.filter((opt) => opt.value !== "rank_any_text")
-    // }
-    // if (!query.query.filters!.bookmarks?.restrict_to_bookmarks) {
-    //     orderOptions = orderOptions.filter((opt) => opt.value !== "time_added")
-    // }
+    const orderOptionsMap = orderByData.meta.force_order_by ? availableFilterOrderOptions : [
+        ...orderOptions,
+        ...availableFilterOrderOptions
+    ]
 
     function onOrderByChange(value: string | null) {
         if (value) {
@@ -50,8 +62,8 @@ export function OrderBy() {
             </div>
             <div className="flex flex-row items-center space-x-2 mt-3 w-full justify-left">
                 <ComboBoxResponsive
-                    options={orderOptions}
-                    currentValue={query.order_by![0].order_by}
+                    options={orderOptionsMap}
+                    currentValue={orderByData.meta.actual_order_by}
                     onChangeValue={onOrderByChange}
                     placeholder="Select order by"
                 />
@@ -64,7 +76,7 @@ export function OrderBy() {
                     resetValue="default"
                     currentValue={query.order_by![0].order!}
                     onChangeValue={onOrderChange}
-                    placeholder="Select order"
+                    placeholder="Default Order"
                 />
             </div>
         </div>
