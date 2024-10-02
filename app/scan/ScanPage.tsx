3855945164e1d4558d3bae23cec9e2d3a45690c9
reasv <7143787+reasv@users.noreply.github.com>
2024-10-02 19:16:34 +0200

    Working path scan

--- app/scan/ScanPage.tsx
+++ app/scan/ScanPage.tsx
@@ -2,7 +2,7 @@
 
 import { $api } from "@/lib/api"
 import { useSelectedDBs } from "@/lib/state/database"
-import { keepPreviousData } from "@tanstack/react-query"
+import { keepPreviousData, useQueryClient } from "@tanstack/react-query"
 import { fileScanColumns } from "@/components/table/columns/filescan"
 import { DataTable } from "@/components/table/dataTable"
 import { dataLogColumns } from "@/components/table/columns/datascan"
@@ -12,6 +12,7 @@ import { Group, InputObject, modelColumns, transformData } from "@/components/ta
 import React, { useEffect } from "react"
 import { RowSelectionState } from "@tanstack/react-table"
 import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
 
 export function ScanPage() {
     return (
@@ -19,6 +20,7 @@ export function ScanPage() {
             <div className={'p-4 mx-auto w-full'}>
                 <ScrollArea className="overflow-y-auto">
                     <div className='max-h-[100vh]'>
+                        <FolderLists />
                         <GroupList />
                         <Tabs defaultValue="filescans">
                             <TabsList>
@@ -42,6 +44,137 @@ export function ScanPage() {
         </div>
     )
 }
+
+export function FolderLists() {
+    const [dbs] = useSelectedDBs();
+    const { data, error, isError, refetch, isFetching } = $api.useQuery(
+        "get",
+        "/api/jobs/folders",
+        {
+            params: {
+                query: dbs,
+            },
+        },
+        {
+            placeholderData: keepPreviousData,
+        }
+    );
+    const queryClient = useQueryClient();
+
+    // Local state for editable folder paths
+    const [includedFolders, setIncludedFolders] = React.useState("");
+    const [excludedFolders, setExcludedFolders] = React.useState("");
+
+    // Update the local state whenever data is fetched
+    useEffect(() => {
+        if (data) {
+            setIncludedFolders(data.included_folders.join("\n"));
+            setExcludedFolders(data.excluded_folders.join("\n"));
+        }
+    }, [data]);
+
+    const update = $api.useMutation(
+        "put",
+        "/api/jobs/folders",
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries({
+                    queryKey: [
+                        "get",
+                        "/api/jobs/folders",
+                        {
+                            params: {
+                                query: dbs,
+                            },
+                        },
+                    ],
+                });
+            },
+        }
+    );
+
+    const rescan = $api.useMutation(
+        "post",
+        "/api/jobs/folders/rescan",
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries({
+                    queryKey: [
+                        "get",
+                        "/api/jobs/folders",
+                        {
+                            params: {
+                                query: dbs,
+                            },
+                        },
+                    ],
+                });
+            },
+        }
+    );
+
+    const updateFolders = async () => {
+        update.mutate({
+            params: {
+                query: dbs,
+            },
+            body: {
+                included_folders: includedFolders.split("\n").map((line) => line.trim()).filter((line) => line !== ""),
+                excluded_folders: excludedFolders.split("\n").map((line) => line.trim()).filter((line) => line !== ""),
+            },
+        });
+    };
+
+    const rescanFolders = () => {
+        rescan.mutate({
+            params: {
+                query: dbs,
+            },
+        });
+    };
+
+    return (
+        <>
+            <Tabs className="mb-4" defaultValue="included">
+                <TabsList>
+                    <TabsTrigger value="included">Included Folder Paths</TabsTrigger>
+                    <TabsTrigger value="excluded">Excluded Folder Paths</TabsTrigger>
+                </TabsList>
+                <TabsContent value="included">
+                    <Textarea
+                        className="min-h-40"
+                        placeholder="One path per line. These folders will be scanned for files."
+                        value={includedFolders}
+                        onChange={(e) => setIncludedFolders(e.target.value)}
+                    />
+                </TabsContent>
+                <TabsContent value="excluded">
+                    <Textarea
+                        className="min-h-40"
+                        placeholder="One path per line. These folders will be excluded from the scan."
+                        value={excludedFolders}
+                        onChange={(e) => setExcludedFolders(e.target.value)}
+                    />
+                </TabsContent>
+            </Tabs>
+            <Button
+                className="ml-4 mb-4"
+                variant="outline"
+                onClick={() => updateFolders()}
+            >
+                Save And Scan New Paths
+            </Button>
+            <Button
+                className="ml-4 mb-4"
+                variant="outline"
+                onClick={() => rescanFolders()}
+            >
+                Rescan All Paths
+            </Button>
+        </>
+    );
+}
+
 
 export function GroupList() {
     const { data, error, isError, refetch, isFetching } = $api.useQuery(
