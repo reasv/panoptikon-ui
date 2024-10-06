d1504de51d5fe7a142f7c78c7af601437dcc1a4e
reasv <7143787+reasv@users.noreply.github.com>
2024-10-06 23:00:21 +0200

    Add job data deletion

--- components/scan/JobHistory.tsx
+++ components/scan/JobHistory.tsx
@@ -1,6 +1,6 @@
 import { $api } from "@/lib/api"
 import { useSelectedDBs } from "@/lib/state/database"
-import { keepPreviousData } from "@tanstack/react-query"
+import { keepPreviousData, useQueryClient } from "@tanstack/react-query"
 import { fileScanColumns } from "@/components/table/columns/filescan"
 import { DataTable } from "@/components/table/dataTable"
 import { dataLogColumns } from "@/components/table/columns/datascan"
@@ -8,6 +8,9 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import React from "react"
+import { useToast } from "../ui/use-toast"
+import { RowSelectionState } from "@tanstack/react-table"
+import { Button } from "../ui/button"
 
 export function JobHistory() {
     return (
@@ -41,15 +44,63 @@ export function DataExtractionHistory() {
             placeholderData: keepPreviousData,
         },
     )
+    const { toast } = useToast()
+    const queryClient = useQueryClient()
+    const [selected, setSelected] = React.useState<RowSelectionState>({})
+    const selectedValues = (data || []).filter((_, index) => selected[index] === true)
+    const deleteJobData = $api.useMutation(
+        "delete",
+        "/api/jobs/data/history", {
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [
+                    "get",
+                    "/api/jobs/data/history",
+                    {
+                        params: {
+                            query: dbs,
+                        },
+                    },
+                ],
+            })
+            toast({
+                title: "Scheduled Data Deletion",
+                description: "The selected data deletion has been scheduled.",
+            })
+            setSelected({})
+        }
+    })
 
+    const deleteSelected = () => {
+        deleteJobData.mutate({
+            params: {
+                query: {
+                    ...dbs,
+                    log_ids: selectedValues.map((job) => job.id),
+                },
+            },
+        })
+    }
     return (
         <ScrollArea className="max-w-[95vw] whitespace-nowrap">
             <DataTable
+                setRowSelection={setSelected}
+                rowSelection={selected}
                 storageKey="dataextraction"
                 data={data || []}
                 columns={dataLogColumns}
                 filterColumn="setter"
                 filterPlaceholder="Search model..."
+                header={
+                    <Button
+                        disabled={selectedValues.length === 0}
+                        className="ml-4"
+                        variant="destructive"
+                        onClick={() => deleteSelected()}
+                    >
+                        Delete Saved Data
+                    </Button>
+                }
             />
             <ScrollBar orientation="horizontal" />
         </ScrollArea>
--- components/table/columns/datascan.tsx
+++ components/table/columns/datascan.tsx
@@ -3,9 +3,32 @@ import { estimateEta, prettyPrintDate, prettyPrintDuration, prettyPrintDurationB
 import { Button } from "@/components/ui/button"
 import { ArrowUpDown } from "lucide-react"
 import { components } from "@/lib/panoptikon"
+import { Checkbox } from "@/components/ui/checkbox"
 
 
 export const dataLogColumns: ColumnDef<components["schemas"]["LogRecord"]>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
     {
         id: "id",
         accessorKey: "id",
