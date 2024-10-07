eea2e1b70fa86650004ec29cb318e52fbb8a6e7e
reasv <7143787+reasv@users.noreply.github.com>
2024-10-07 02:35:11 +0200

    Add link to search page

--- app/scan/ScanPage.tsx
+++ app/scan/ScanPage.tsx
@@ -7,13 +7,32 @@ import { GroupList } from "@/components/scan/GroupLists"
 import { JobQueue } from "@/components/scan/JobQueue"
 import { JobHistory } from "@/components/scan/JobHistory"
 import { FolderLists } from "@/components/scan/FolderLists"
+import { Button } from "@/components/ui/button"
+import { SidebarClose } from "lucide-react"
+import Link from "next/link"
+import { selectedDBsSerializer, useSelectedDBs } from "@/lib/state/database"
+import { useMemo } from "react"
 
 export function ScanPage() {
+  const dbs = useSelectedDBs()[0]
+  const searchLink = useMemo(() => {
+    return selectedDBsSerializer("/search", {
+      index_db: dbs.index_db,
+      user_data_db: dbs.user_data_db,
+    })
+  }, [dbs])
   return (
     <div className="flex w-full h-screen">
       <div className={"p-4 mx-auto w-full"}>
         <ScrollArea className="overflow-y-auto">
           <div className="max-h-[100vh] mr-4">
+            <Link
+              href={searchLink}
+            >
+              <Button title="Back to Search" variant="ghost" size="icon">
+                <SidebarClose className="h-4 w-4" />
+              </Button>
+            </Link>
             <div className='grid gap-4 grid-cols-1 lg:grid-cols-2'>
               <SwitchDB />
               <CreateNewDB />
