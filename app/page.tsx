02bd1a32af284a93aab8551bda69dc01388ba88a
reasv <7143787+reasv@users.noreply.github.com>
2024-10-08 02:33:14 +0200

    Center elements

--- app/page.tsx
+++ app/page.tsx
@@ -4,27 +4,27 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          src="/spinner_text.svg"
-          alt="Panoptikon logo"
-          width={256}
-          height={256}
-        />
-        <div className="flex flex-col items-left">
-          <div className="flex flex-row items-center justify-between">
-            <div className="space-y-0.5">
-              <Label className="text-2xl">
-                Getting Started
-              </Label>
-              <div className="text-gray-400 text-xl">
-                Welcome to Panoptikon! This is a tool for indexing and searching files on your computer using AI.
-              </div>
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
+      <main className="flex flex-col gap-8 items-center text-center sm:text-left max-w-lg">
+        <div className="flex justify-center w-full">
+          <Image
+            src="/spinner_text.svg"
+            alt="Panoptikon logo"
+            width={256}
+            height={256}
+          />
+        </div>
+        <div className="flex flex-col">
+          <div className="space-y-0.5">
+            <Label className="text-2xl">
+              Getting Started
+            </Label>
+            <div className="text-gray-400 text-xl">
+              Welcome to Panoptikon! This is a tool for indexing and searching files on your computer using AI.
             </div>
           </div>
-          <div className="flex flex-row items-center space-x-2 mt-4 w-full justify-center">
-            <p>
+          <div className="mt-4 w-full">
+            <p className="text-left">
               First, you need to add some directories to scan.<br />
               Visit the <Link href={"/scan"} className="underline">Scan</Link> page, paste the directories you want to scan
               into the <b>Included Directories</b> textbox, and click <b>Save And Scan New Paths</b>.
@@ -49,6 +49,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-
   );
 }
