88c502f4f1e6437946b1c2b97d3f8771e465d745
reasv <7143787+reasv@users.noreply.github.com>
2024-10-11 06:04:07 +0200

    Add pins to element

--- components/GalleryPinBoard.tsx
+++ components/GalleryPinBoard.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { cn, getFullFileURL, getFullFileURLFromFileID, getThumbnailURL, getThumbnailURLFromFileID } from "@/lib/utils";
 import { useSelectedDBs } from "@/lib/state/database";
+import { useGalleryPins } from '@/lib/state/gallery';
+import { PinButton } from './PinButton';
 
 export function PinBoard(
     {
@@ -14,18 +16,37 @@ export function PinBoard(
     const [dbs, ___] = useSelectedDBs()
     const thumbnailURL = getThumbnailURLFromFileID(selectedItem.file_id, dbs)
     const fileURL = getFullFileURLFromFileID(selectedItem.file_id, dbs)
-
+    const [pins, setPins] = useGalleryPins()
     return (
         <div
             className={cn("relative flex-grow flex justify-center items-center overflow-hidden ",
                 thumbnailsOpen ? "h-[calc(100vh-570px)]" : "h-[calc(100vh-215px)]" // Set height based on whether thumbnails
             )}
         >
+            {pins.map((pin) => (
+                <a
+                    href={getFullFileURLFromFileID(pin, dbs)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="absolute inset-0 group"
+                    onClick={(e) => e.preventDefault()}
+                >
+                    <Image
+                        key={pin}
+                        src={getThumbnailURLFromFileID(pin, dbs)}
+                        alt={`File ID ${pin}`}
+                        fill
+                        className="object-contain"
+                        unoptimized={true}
+                    />
+                    <PinButton file_id={pin} />
+                </a>
+            ))}
             <a
                 href={fileURL}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="absolute inset-0"
+                className="absolute inset-0 group"
                 onClick={(e) => e.preventDefault()}
             >
                 <Image
@@ -35,6 +56,7 @@ export function PinBoard(
                     className="object-contain"
                     unoptimized={true}
                 />
+                <PinButton file_id={selectedItem.file_id} />
             </a>
         </div>
     )
