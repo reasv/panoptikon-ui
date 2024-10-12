44e620f4c3fcee6714be64f8d04fceba1ab96cd2
reasv <7143787+reasv@users.noreply.github.com>
2024-10-12 16:22:24 +0200

    Single layout

--- components/GalleryPinBoard.tsx
+++ components/GalleryPinBoard.tsx
@@ -83,8 +83,8 @@ export function PinBoard(
                 <ResponsiveGridLayout
                     className="layout"
                     layouts={{ lg: layout }}
-                    breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-                    cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                    breakpoints={{ lg: 0, }}
+                    cols={{ lg: 12, }}
                     rowHeight={150}
                     onLayoutChange={(currentLayout) => onLayoutChange(currentLayout)}
                     draggableHandle=".drag-handle"
