a4a7a30c50f60d9cbc38bd58928b027e075ba334
reasv <7143787+reasv@users.noreply.github.com>
2024-11-09 07:59:21 +0100

    Disable react compiler for virtualized element list

--- components/gallery/VirtualizedHorizontalScroll.tsx
+++ components/gallery/VirtualizedHorizontalScroll.tsx
@@ -22,6 +22,7 @@ export function VirtualGalleryHorizontalScroll({
 }: {
     items: SearchResult[]
 }) {
+    "use no memo"
     const parentRef = useRef<HTMLDivElement>(null)
 
     const virtualizer = useVirtualizer({
